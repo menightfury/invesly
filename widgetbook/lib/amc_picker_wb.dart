b3d24895d0e0fcd2456f1af96cc219ff5b79385a
MeNightfury <me.nightfury@gmail.com>
2026-08-17 16:27:01 +0530

    Remove widgetbook web project files and configuration

--- widgetbook/lib/amc_picker_wb.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:invesly/amcs/view/widgets/amc_picker_widget.dart';
-import 'package:widgetbook_annotation/widgetbook_annotation.dart';
-
-@UseCase(name: 'Default', type: InveslyAmcPickerWidget)
-Widget buildAmcPickerUseCase(BuildContext context) {
-  return InveslyAmcPickerWidget();
-}
